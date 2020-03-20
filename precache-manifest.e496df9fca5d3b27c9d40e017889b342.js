4926aa6ca650400c66dafffd36091f5b2f956557
Gurpreet <gurpreet1161@gmail.com>
2020-03-20 17:45:40 +1200

    Updates

---
+++ precache-manifest.e496df9fca5d3b27c9d40e017889b342.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee4eae50c01094c3a01b6636904f29dd",
+    "url": "/sparkleautoservice/index.html"
+  },
+  {
+    "revision": "059d1a8b7eb03591683d",
+    "url": "/sparkleautoservice/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "1328b17de6a762211f13",
+    "url": "/sparkleautoservice/static/css/main.5b3ff61f.chunk.css"
+  },
+  {
+    "revision": "059d1a8b7eb03591683d",
+    "url": "/sparkleautoservice/static/js/2.98b775c5.chunk.js"
+  },
+  {
+    "revision": "2b8cc04d3b239d52c1e995484cbcd9e3",
+    "url": "/sparkleautoservice/static/js/2.98b775c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1328b17de6a762211f13",
+    "url": "/sparkleautoservice/static/js/main.8b987752.chunk.js"
+  },
+  {
+    "revision": "8b98fe0440a5552058d5",
+    "url": "/sparkleautoservice/static/js/runtime-main.6d194d0c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sparkleautoservice/static/media/logo.5d5d9eef.svg"
+  }
+]);
