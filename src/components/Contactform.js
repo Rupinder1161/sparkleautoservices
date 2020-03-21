0eb176e7488ffa349da014a1e727b300c000a816
Gurpreet <gurpreet1161@gmail.com>
2020-03-21 14:35:45 +1200

    form upload

--- src/components/Contactform.js
+++ src/components/Contactform.js
@@ -2,12 +2,20 @@ import React, { Component } from 'react'
 import './hh.css'
 
 
+
 export class Contactform extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { name: "", email: "", message: "" };
+      }
+
+
     render() {
         return (
             <div className="hello" >  
             
-             <form id="contact" name="contact" method="POST" data-netlify="true">
+             <form id="contact" name="contact" method="POST">
+                 <input type="hidden" name="form-name" value="contact" />
               <h3> Contact Form</h3>
               <h4>Contact us for custom quote</h4>
               <fieldset>
@@ -17,13 +25,17 @@ export class Contactform extends Component {
                   <input placeholder="Your Conatct Number" type="tel" tabindex="1" required autofocus />
               </fieldset>
               <fieldset>
-                  <input placeholder="Your Vehicle Model" type="tel" tabindex="1" required autofocus />
+                  <input placeholder="Your Vehicle Model" type="text" tabindex="1" required autofocus />
               </fieldset>
               <fieldset>
                 <textarea placeholder="Type your message here...." tabindex="5" required></textarea>
               </fieldset>
               <fieldset>
-                <button name="submit" type="submit" id="contact-submit" >Submit</button>
+                <button name="submit"
+                 type="submit"
+                //  id="contact-submit" 
+                
+                >Submit</button>
               </fieldset>
               <p class="copyright">Or Contact Us On <a href="https://colorlib.com" target="_blank" title="Colorlib">04 26 00 116</a></p>
                           </form>
